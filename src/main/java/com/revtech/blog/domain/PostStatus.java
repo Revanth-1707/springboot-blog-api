25bc74f8877383e77bd5e6d4fea2eb5642298132
Revanth-1707 <revanthagastya373@gmail.com>
2025-06-18 09:31:46 +0530

    Initial commit (history cleared)

---
+++ src/main/java/com/revtech/blog/domain/PostStatus.java
@@ -0,0 +1,5 @@
+package com.revtech.blog.domain;
+
+public enum PostStatus {
+	DRAFT, PUBLISHED
+}
---
+++ src/main/java/com/revtech/blog/domain/entities/User.java
@@ -0,0 +1,76 @@
+package com.revtech.blog.domain.entities;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "users")
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.UUID)
+	private UUID id;
+	
+	@Column(nullable = false, unique = true)
+	private String email;
+	
+	@Column(nullable = false)
+	private String password;
+	
+	@Column(nullable = false)
+	private String name;
+	
+	@Column(nullable = false)
+	private LocalDateTime createdAt;
+	
+	@OneToMany(mappedBy = "author", cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<Post> posts = new ArrayList<>();
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(createdAt, email, id, name, password);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(createdAt, other.createdAt) && Objects.equals(email, other.email)
+				&& Objects.equals(id, other.id) && Objects.equals(name, other.name)
+				&& Objects.equals(password, other.password);
+	}
+	
+	@PrePersist
+	protected void  onCreate() {
+		this.createdAt = LocalDateTime.now();
+	}
+	
+}
